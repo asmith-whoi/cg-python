43bb2919d649789bd0cc3ac4fc003197f198fa11
Allen <allen@whoi.edu>
2018-08-31 15:19:56 -0400

    Pads numbers with zeros to make ids

--- test_many_uimms.py
+++ test_many_uimms.py
@@ -1,9 +1,14 @@
+import imm
 import cgserial as ser
 
-port = ser.select_port_win()
-conn = ser.open_port(port, "9600")
+#port = ser.select_port_win()
+#conn = ser.open_port(port, "9600")
 
-tot_uimm = raw_input("How many UIMMs have you?")
-raw_input("Are all id's set consecutively starting with 01? Press enter to continue...")
+user_response = raw_input("How many UIMMs have you?")
 
+raw_input("Are all id's set consecutively starting with 01? Press 'Enter' to continue...")
+iids = [*range(1, user_response+1)]
+imm.num_to_id(iids)
 
+for iid in iids
+    print("Looking for UIMM id%s..." % iid)
