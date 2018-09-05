d3788e13d54e88e0669cb0bc72df2fc47ffdabb3
Allen <allen@whoi.edu>
2018-09-05 12:49:59 -0400

    Included helper file to validate user supplied integers.

--- test_many_uimms.py
+++ test_many_uimms.py
@@ -1,14 +1,20 @@
 import imm
-import cgserial as ser
+from datetime import datetime
+
+import cg_util as cu
+#import cgserial as ser
 
 #port = ser.select_port_win()
 #conn = ser.open_port(port, "9600")
 
-user_response = raw_input("How many UIMMs have you?")
+input("Are all id's set consecutively starting with 01? Press 'Enter' to continue...")
 
-raw_input("Are all id's set consecutively starting with 01? Press 'Enter' to continue...")
-iids = [*range(1, user_response+1)]
+validated_input = cu.prompt_integer("How many UIMM have you?",range(1,100))
+iids = [*range(1,validated_input+1)]
 imm.num_to_id(iids)
 
-for iid in iids
-    print("Looking for UIMM id%s..." % iid)
+# Let's make log files for each id...
+logdir = datetime.today().strftime("%Y%m%d")
+
+for iid in iids:
+    print("Opening ID%s_logfile.txt..." % iid)
