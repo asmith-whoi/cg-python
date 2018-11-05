ec2a30c3e5d75d31e943fc5f70c9f1e83e6af523
Allen <allen@whoi.edu>
2018-11-05 07:04:17 -0500

    New script to process phsen data log files. Added code to find and join pieces of data records.

---
+++ cgphsen.py
@@ -0,0 +1,25 @@
+import cgutil
+import cgsami
+
+def load_log_file(infile):
+    lines = cgutil.dlog_scrub_logfile_asc(infile)
+
+    phsamples = []
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("*") and lines[i][5:7] == "0A":
+            sample = ""
+            while True:
+                try:
+                    sample = sample + lines[i]
+                except IndexError:
+                    break
+                i = i+1
+                if  len(sample) == 465:
+                    phsamples.append(sample)
+                    break
+                
+        else:
+            i = i + 1
+
+    return phsamples
