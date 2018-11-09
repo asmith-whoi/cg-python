0720a0db076923908c090145ea66a9d75c6167ec
Allen <allen@whoi.edu>
2018-11-09 08:14:05 -0500

    Added temperature processing fumction. Needs debugging and testing

--- cgphsen.py
+++ cgphsen.py
@@ -1,5 +1,7 @@
 import cgutil
-from cgsami import SAMIRecord
+import cgplot
+from cgsami import SAMIRecord, PHSENRecord
+from matplotlib import pyplot as plt
 
 def load_log_file(infile):
     lines = cgutil.dlog_scrub_logfile_asc(infile)
@@ -30,3 +32,13 @@ def load_phsamples(samples):
         phrecord = SAMIRecord(sample)
         phrecords.append(phrecord)
     return phrecords
+
+def plot_phbatt(records):
+    t, batt = [], []
+    for record in records:
+        t.append(record.telltime())
+        batt.append(record.batt)
+    fig, ax = plt.subplots(1, 1)
+    cgplot.cgplotter(ax, t, batt, {'Label':'Battery'})
+    return
+
